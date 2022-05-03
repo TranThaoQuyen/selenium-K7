5cb995502b6e93cde15a302e054212db502de050
Tran Thao Quyen <97901747+TranThaoQuyen@users.noreply.github.com>
2022-05-04 00:18:00 +0700

    launch browser

--- src/test/java/test/LaunchBrowser.java
+++ src/test/java/test/LaunchBrowser.java
@@ -1,7 +1,27 @@
 package test;
 
-public class LaunchBrowser {
-    public static void main(String[] args) {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+public class LaunchBrowser {
+    public static void main(String[] args) throws InterruptedException {
+        String currentProjectLocation = System.getProperty("user.dir");
+        String relativeChromeDriverPath = "/src/test/resources/drivers/chromedriver.exe";
+        String chromeDriverPath = currentProjectLocation.concat(relativeChromeDriverPath);
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://sdetpro.com/");
+        driver.manage().window().maximize();
+
+        //DEBUG PURPOSE ONLY
+        Thread.sleep(3000);
+
+        //QUIT SESSION
+        driver.quit();
     }
 }
